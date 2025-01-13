17a88c39b26aeb74bf3a4a0bd5ed62616f694268
æLtorio <aeltorio@universidadatlantico.org>
2025-01-13 12:52:44 +0100

    bug

--- upgrade-bitnami-16-to-17.sh
+++ upgrade-bitnami-16-to-17.sh
@@ -5,5 +5,5 @@ mkdir /bitnami/postgresql/data
 pg_ctl initdb -D /bitnami/postgresql/data
 cp /bitnami/postgresql/data/*.conf /bitnami/postgresql/data16/
 chmod -R 0750 /bitnami/postgresql/data16
-pg_upgrade -b /usr/lib/postgresql/15/bin --old-datadir /bitnami/postgresql/data16 --new-datadir /bitnami/postgresql/data -B /usr/lib/postgresql/16/bin
+pg_upgrade -b /usr/lib/postgresql/16/bin --old-datadir /bitnami/postgresql/data16 --new-datadir /bitnami/postgresql/data -B /usr/lib/postgresql/17/bin
 rm -rf /bitnami/postgresql/data/pg_upgrade_output.d/*
